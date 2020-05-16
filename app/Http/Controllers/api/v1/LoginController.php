6a5af547522121caa24235d5e3b4aef8ed22bec7
acgh1 <acgh1@agencycanoe.com>
2020-05-17 02:53:05 +0600

    all files updated

---
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $input = $this->validate($request, [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:6',
+        ], [
+            'email.exists' => 'The user credentials were incorrect.',
+        ]);
+
+        request()->request->add([
+            'grant_type' => 'password',
+            'client_id' => env('PASSWORD_CLIENT_ID'),
+            'client_secret' => env('PASSWORD_CLIENT_SECRET'),
+            'username' => $input['email'],
+            'password' => $input['password'],
+        ]);
+        $response = Route::dispatch(Request::create('/oauth/token', 'POST'));
+        $data = json_decode($response->getContent(), true);
+        if (!$response->isOk()) {
+            return response()->json($data, 401);
+        }
+        return $data;
+    }
+
+    public function logout(Request $request)
+    {
+        $accessToken = $request->user()->token();
+
+        DB::table('oauth_refresh_tokens')
+            ->where('access_token_id', $accessToken->id)
+            ->update([
+                'revoked' => true,
+            ]);
+
+        $accessToken->revoke();
+
+        return response()->json([], 201);
+    }
+}
